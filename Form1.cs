b2761c1ae690aba3d10fbe0c506856467b7d4548
MattTheViking3110 <88708780+MattTheViking3110@users.noreply.github.com>
2021-08-25 08:30:35 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VaccineShit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection con = new SqlConnection(@"Data Source =.; Initial Catalog = Vaccine; Integrated Security = True");
+            SqlCommand cmd = new SqlCommand("Insert Into VaccineRegs(ID, FName, LName, Gender) Values ('" + this.txbID.Text + "', '" + this.txbFName.Text + "', '" + this.txbLName.Text + "', @gender);", con);
+            cmd.Parameters.AddWithValue("@gender", cbxGender.SelectedItem);
+            con.Open();
+            try { 
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully registered");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid action");
+            }
+            con.Close();
+           
+        }
+    }
+}
